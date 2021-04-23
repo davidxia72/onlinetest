d05e5760b18d3218c1e4942459cdd0dddfc72001
CORPADDS\P12529D <david.xia@ceridian.com>
2021-04-22 21:43:49 -0400

    add stack

---
+++ ConsoleApp1/CsharpStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class CsharpStack
+    {
+        private int p_index;
+        private ArrayList list;
+        public CsharpStack()
+        {
+            list = new ArrayList();
+            p_index = -1;
+        }
+        public int count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+        public void push(object item)
+        {
+            list.Add(item);
+            p_index++;
+        }
+        public object pop()
+        {
+            object obj = list[p_index];
+            list.RemoveAt(p_index);
+            p_index--;
+            return obj;
+        }
+        public void clear()
+        {
+            list.Clear();
+            p_index = -1;
+        }
+        public object peek()
+        {
+            return list[p_index];
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World, first branch!");
+            CsharpStack alist = new CsharpStack();
+            string ch;
+            string word = "eye";
+            bool isPalindrome = true;
+            for (int x = 0; x < word.Length; x++)
+                alist.push(word.Substring(x, 1));
+            int pos = 0;
+            while (alist.count > 0)
+            {
+                ch = alist.pop().ToString();
+                if (ch != word.Substring(pos, 1))
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                pos++;
+            }
+            if (isPalindrome)
+                Console.WriteLine(word + " is a palindrome.");
+            else
+                Console.WriteLine(word + " is not a palindrome.");
+            Console.Read();
         }
     }
 }
